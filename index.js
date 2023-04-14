2be0e61321d997cf4ad98058f86fa342cfee06c1
Ivo Augusto Wanderley de Paiva <40743787+ivomastre@users.noreply.github.com>
2023-04-14 09:46:56 -0300

    chore: added a http server

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 import SteamClient from "./steamClient.js";
 import updateDiscordPost from "./updateDiscordPost.js";
 import "dotenv/config";
+import http from "http";
 
-const ADDRESS = process.env.SERVER_ADDRESS;
-const PORT = process.env.SERVER_PORT;
+const SERVER_ADDRESS = process.env.SERVER_ADDRESS;
+const SERVER_PORT = process.env.SERVER_PORT;
+const PORT = process.env.PORT || 3000;
 
 const main = async () => {
   const steamClient = new SteamClient();
-  const serverInfo = await steamClient.fetchServer(ADDRESS, PORT);
+  const serverInfo = await steamClient.fetchServer(SERVER_ADDRESS, SERVER_PORT);
 
   await updateDiscordPost(serverInfo);
 };
 
-main();
+const server = http.createServer((_, res) => {
+  main();
+
+  res.end();
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} 🐒`);
+});
